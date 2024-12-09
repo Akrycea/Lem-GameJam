a3407d877863c0689773976e8a929ac5fce01823
Frankiplier <agatarucka@gmail.com>
2024-12-09 20:12:56 +0100

    Created main menu

---
+++ Lem_GameJam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string sceneName;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true; 
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
