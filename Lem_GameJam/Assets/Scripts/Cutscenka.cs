2baa438e11a0c919d40cbcff9c9dc3f7b8d913a1
Frankiplier <agatarucka@gmail.com>
2025-02-08 14:39:33 +0100

    Added skip cutscene option

--- Lem_GameJam/Assets/Scripts/Cutscenka.cs
+++ Lem_GameJam/Assets/Scripts/Cutscenka.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Cutscenka : MonoBehaviour
 {
+    public GameObject canvas;
 
     void Start()
     {
+        canvas.SetActive(false);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -15,11 +17,18 @@ public class Cutscenka : MonoBehaviour
     private void Update()
     {
         StartCoroutine(wait());
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 
     IEnumerator wait()
     {
-        yield return new WaitForSeconds(12);
+        yield return new WaitForSeconds(3);
+        canvas.SetActive(true);
+        yield return new WaitForSeconds(9);
         SceneManager.LoadScene(2);
     }
 
